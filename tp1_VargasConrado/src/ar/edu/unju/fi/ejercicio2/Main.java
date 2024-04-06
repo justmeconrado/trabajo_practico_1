4cc9c91ea137a1c30e7b1e125fd092156eb423f8
Conrado Vargas <conradovargas93@gmail.com>
2024-04-06 00:39:55 -0300

    Ejercicio 2 Finalizado

---
+++ tp1_VargasConrado/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Declaración de las variables
+
+		String nombrePais = "Malasia";
+		int edad = 27;
+		double altEdificio = 678.9;
+		double precProducto = 99.99;
+		long numTel = 5493884123456L;
+		double cosDeMedio = Math.cos(0.5);
+		
+        // Mostrar los valores por consola
+        System.out.println("Nombre del país: " + nombrePais);
+        System.out.println("Edad de la persona: " + edad + " años");
+        System.out.println("Altura del edificio: " + altEdificio + " metros");
+        System.out.println("Precio del producto: $" + precProducto);
+        System.out.println("Número de teléfono: +" + numTel);
+        System.out.println("Coseno de 0.5: " + cosDeMedio);
+    }
+}
