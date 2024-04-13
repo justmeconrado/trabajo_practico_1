ee4973fa9050ec0dfcbb14698d2b9e0d3619d399
Conrado Vargas <conradovargas93@gmail.com>
2024-04-13 03:34:59 -0300

    Asistencia 12/04 - TP 1 continuación - Ejercicio 11 finalizado

---
+++ tp1_VargasConrado/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+	public static void main(String[] args) {
+		int j = 40; // Inicialización de j
+		int i = 2; // Inicialización de i
+		int resultado; // Inicialización de resultado
+
+		// Verificar si el valor de j es negativo
+		for (; j >= 0; j--) {
+
+			resultado = j * i;
+
+			System.out.print(" " + resultado);
+			i++;
+		}
+	}
+}
+
+// Resultado esperado:80 117 152 185 216 245 272 297 320 341 360 377 392 405 416
+// 425 432 437 440 441 440 437 432 425 416 405 392 377 360 341 320 297 272 245
+// 216 185 152 117 80 41
